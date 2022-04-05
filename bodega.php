ab27efea8478962d845f54da72a820017e1b997f
vipsum <83631487+vipsum@users.noreply.github.com>
2022-04-05 01:39:43 -0300

    ejercicio de bodega

---
+++ bodega.php
@@ -0,0 +1,29 @@
+<?php
+$edades = array(1, 5, 7, 2, 6, 10);
+
+function bodega_correspondiente($arrayEdades){
+    foreach ($arrayEdades as $edad) {
+       switch ($edad) {
+           case $edad > 0 && $edad < 5:
+              concatenate($edad, "inferior");
+               break;
+            case $edad >= 5 && $edad < 7:
+                concatenate($edad, "media");
+                break;
+            case $edad > 7:
+                concatenate($edad, "alta");
+                break;
+
+            default:
+                echo "El estudiante con edad indefinida tiene un espacio en la bodega de al lado.". PHP_EOL;
+                break;
+       }
+    }
+   
+}
+
+function concatenate ($edad, $espacio){
+  echo "El estudiante de $edad años tendra sus juguetes en la parte $espacio de la bodega.". PHP_EOL;
+}
+bodega_correspondiente($edades);
+?>
